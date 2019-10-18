57bceb2372cefaf0f0639856eac2f8a7e8358b09
Mitchel <mitchelwalker219@gmail.com>
2019-10-18 10:14:46 +0000

    Adjusted PWM_turn.py to work with motor

--- stepper_motors/PWM_turn.py
+++ stepper_motors/PWM_turn.py
@@ -2,6 +2,7 @@
 #instead of software-based time.sleep
 
 import pigpio
+from time import sleep
 
 
 #set pin numbers
@@ -15,14 +16,23 @@ pi = pigpio.pi()
 
 #set duty cycle and frequency
 pi.set_PWM_dutycycle(step_pin, 128)
-pi.set_PWM_frequency(step_pin,500)
+pi.set_PWM_frequency(step_pin,20000)
+'''
+acceptable frequencies:
+1: 40000 20000 10000 8000 5000 4000 2500 2000 1600
+2: 20000 10000 5000  4000 2500 2000 1250 1000 800
+4: 10000 5000  2500  2000 1250 1000 625  500  400
+5: 8000  4000  2000  1600 1000 800  500  400  320
+8: 5000  2500  1250  1000 625  500  313  250  200
+10:4000  2000  1000  800  500  400  250  200  160
+'''
 
 try:
 	while True:
 		pi.write(dir_pin, cw)
 		sleep(.1)
 except KeyboardInterrupt:
-	print("^CKeyboard Pressed")
+	print("Keyboard Pressed")
 finally:
 	pi.set_PWM_dutycycle(step_pin,0)
 	pi.stop()
