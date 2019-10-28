79ac9ff84049d1071ae877e533bc3de15853c0cd
Mitchel <mitchelwalker219@gmail.com>
2019-10-28 10:44:01 -0500

    Updated simple_turn.py

--- stepper_motors/simple_turn.py
+++ stepper_motors/simple_turn.py
@@ -11,3 +11,19 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(dir_pin, GPIO.OUT)
 GPIO.setup(step_pin, GPIO.OUT)
 GPIO.output(dir_pin, cw)
+
+#set mircrostep rev pins
+mode = (14,15,18)
+GPIO.setup(GPIO.OUT)
+
+#set output resolution
+GPIO.output(mode, (1,0,1))
+
+step_count = steps_per_rev*32
+delay = 0.0208/32
+
+#loop through
+for i in range(step_count):
+	GPIO.output(step_pin, GPIO.HIGH)
+	sleep(delay)
+	GPIO.output(step_pin, GPIO.LOW)
