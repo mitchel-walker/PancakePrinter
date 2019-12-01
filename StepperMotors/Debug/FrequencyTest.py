4cbb3a354b1c96031bf4270f604459b470661605
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-01 13:32:54 -0600

    incremental changes

---
+++ StepperMotors/Debug/FrequencyTest.py
@@ -0,0 +1,7 @@
+#This program tests the actual frequency limits of software-based pwm generation
+from time import time, sleep
+
+if __name__ == "__main__":
+	ideal_time = input("Enter desired frequency: ")
+
+	
