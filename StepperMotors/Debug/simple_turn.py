4a70894f740bb03a712dee91ade8bc06bde6626e
mitchel-walker <mitchelwalker219@gmail.com>
2019-11-30 19:47:52 -0600

    changes to debug files to test microstepping

--- StepperMotors/Debug/simple_turn.py
+++ StepperMotors/Debug/simple_turn.py
@@ -31,7 +31,7 @@ resolution = {'full':(0,0,0),
 
 
 #set output resolution
-GPIO.output(mode, resolution['1/4'])
+GPIO.output(mode, resolution['half'])
 
 step_count = steps_per_rev*32
 delay = 0.005
