7973703149d9716e17066463bbe919b342c67014
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-01 13:44:39 -0600

    Incremental Changes

--- StepperMotors/InterpretGCode/Devices.py
+++ StepperMotors/InterpretGCode/Devices.py
@@ -84,12 +84,15 @@ class Motor():
 		#set delay time
 		delay = sec/(num_pulses*2)
 
+		print(num_pulses)
+
 		i  = 0
 		while i < num_pulses:
 			gpio.output(self.step_pin, gpio.HIGH)
 			sleep(delay)
 			gpio.output(self.step_pin, gpio.LOW)
 			i+= 1
+			print(i)
 			
 		'''
 		print(freq)
