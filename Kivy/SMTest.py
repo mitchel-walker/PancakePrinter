25116fc95b114f4c92e66f207fabf47e7bedbac7
mitchel-walker <mitchelwalker219@gmail.com>
2019-11-30 18:06:06 -0600

    Changes to UI

--- Kivy/SMTest.py
+++ Kivy/SMTest.py
@@ -1,17 +1,48 @@
-#This is a test for the use of the screen Manager
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
 
-class Screen1(Screen):
+
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: 'Goto settings'
+            on_press: root.manager.current = 'settings'
+        Button:
+            text: 'Quit'
+
+<SettingsScreen>:
+    BoxLayout:
+        Button:
+            text: 'My settings button'
+        Button:
+            text: 'Back to menu'
+            on_press: root.manager.current = 'menu'
+""")
+
+# Declare both screens
+class MenuScreen(Screen):
 	pass
 
-
-class Screen2(Screen):
+class SettingsScreen(Screen):
 	pass
 
-
-
+# Create the screen manager
 sm = ScreenManager()
+sm.transition = "no_transition"
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
 
+class TestApp(App):
 
+    def build(self):
+        return sm
+
+if __name__ == '__main__':
+    TestApp().run()
