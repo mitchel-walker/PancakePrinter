78538317324a691bfa7f4a20823b1565fe8741aa
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-02 23:36:29 -0600

    updated files for configuring motor motion

--- StepperMotors/InterpretGCode/Devices.py
+++ StepperMotors/InterpretGCode/Devices.py
@@ -38,7 +38,7 @@ class Motor():
 		}
 
 		#set object resolution string and microstep pin values
-		self.set_resolution('1/8')
+		self.set_resolution('half')
 
 
 	#must initialize pwm object with duty cycle 0 (arbitrary frequency 100 is set)
