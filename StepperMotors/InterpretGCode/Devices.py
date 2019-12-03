ae2632450e69ab2bea55613ebec58742d9e4a3fe
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-03 02:47:07 -0600

    updated files for configuring motor motion

--- StepperMotors/InterpretGCode/Devices.py
+++ StepperMotors/InterpretGCode/Devices.py
@@ -130,6 +130,7 @@ class Motor():
 			return
 		#pause for time if dist = 0
 		if dist == 0:
+			print("sleeping for time: " + str(sec))
 			sleep(sec)
 			return
 
@@ -153,13 +154,16 @@ class Motor():
 		#get acceleration pulses
 		acc_pulses = self.accelerate((dist/sec), sec)
 
-
+		start_time = time()
 		i  = 0
 		while i < num_pulses-acc_pulses:
 			gpio.output(self.step_pin, gpio.HIGH)
 			sleep(delay)
 			gpio.output(self.step_pin, gpio.LOW)
 			i+= 1
+		end_time = time()
+
+		print("actual time: " + str(end_time-start_time))
 			
 
 
