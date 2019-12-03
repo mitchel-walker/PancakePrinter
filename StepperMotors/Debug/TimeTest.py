27fec374a90c11ac8e12c92e6b91bae47c33b436
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-03 03:00:34 -0600

    updated files for configuring motor motion

--- StepperMotors/Debug/TimeTest.py
+++ StepperMotors/Debug/TimeTest.py
@@ -1,5 +1,6 @@
 #This program is meant to test the accuracy of python's time.sleep on the raspberry pi
 from time import sleep, time
+import RPi.GPIO as gpio
 
 def timer(length, delay):
 	loop_limit = int(length/delay)
@@ -16,14 +17,16 @@ def timer(length, delay):
 	print("actually turned for %f seconds" % stop)
 
 def time_while():
+	step_pin = 21
+
 	num_pulses = 11
 	acc_pulses = 1
 	i  = 0
 	start_time = time()
 	while i < num_pulses-acc_pulses:
-		gpio.output(self.step_pin, gpio.HIGH)
+		gpio.output(step_pin, gpio.HIGH)
 		sleep(delay)
-		gpio.output(self.step_pin, gpio.LOW)
+		gpio.output(step_pin, gpio.LOW)
 		i+= 1
 
 	return (time()-start_time)/10
@@ -35,6 +38,10 @@ if __name__ == "__main__":
 	delay = eval(input("Input delay time: "))
 	timer(10, delay)
 	'''
+	gpio.setmode(gpio.BCM)
+	
+	gpio.setup(21, gpio.OUT)
+
 	sm = 0
 	for i in range(10):
 		sm += time_while()
