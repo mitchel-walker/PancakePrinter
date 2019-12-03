10949d15a8da6741ed15cbb4919827b6640d7cc5
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-03 02:52:04 -0600

    updated files for configuring motor motion

--- StepperMotors/Debug/TimeTest.py
+++ StepperMotors/Debug/TimeTest.py
@@ -15,6 +15,16 @@ def timer(length, delay):
 
 	print("actually turned for %f seconds" % stop)
 
+def time_while():
+	num_pulses = 100
+	i  = 0
+		while i < num_pulses-acc_pulses:
+			gpio.output(self.step_pin, gpio.HIGH)
+			sleep(delay)
+			gpio.output(self.step_pin, gpio.LOW)
+			i+= 1
+
+
 
 if __name__ == "__main__":
 	delay = eval(input("Input delay time: "))
