78c24bfa7379eaadc45fd8deacd85ab7c4f11558
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-03 23:10:24 -0600

    incremental upgrades

--- StepperMotors/InterpretGCode/Devices.py
+++ StepperMotors/InterpretGCode/Devices.py
@@ -312,7 +312,7 @@ if __name__ == "__main__":
 	gpio.setup(output_pins,gpio.OUT)
 
 	printer = Printer(config_dict)
-	printer.x.set_resolution('full')
+	printer.x.set_resolution('1/4')
 
 	printer.x.move(100,4,1)
 	printer.x.move(100,4,0)
