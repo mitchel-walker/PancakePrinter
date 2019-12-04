de44b77f1078677a0da20ed12592d3993d82f6b9
mitchel-walker <mitchelwalker219@gmail.com>
2019-12-04 00:42:16 -0600

    incremental upgrades

--- StepperMotors/InterpretGCode/PrintFromFile.py
+++ StepperMotors/InterpretGCode/PrintFromFile.py
@@ -25,10 +25,10 @@ def run(printer, gcd_file):
 			continue
 		elif command[0] == "G00":
 			#first determine if command should be sent
-			x_cudist += (eval(command[1][1:]) - printer.pos[0])
+			x_cudist = x_cudist + (eval(command[1][1:]) - printer.pos[0])
 			y_cudist += (eval(command[2][1:]) - printer.pos[1])
 
-			print((eval(command[1][1:]) - printer.pos[0]))
+			print(x_cudist)
 			
 
 			if (x_cudist >= min_dist) and (y_cudist >= min_dist):
